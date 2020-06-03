228ba603ba58a016d70821f4e34bc4a2831d5ede
bratiGIT <48324883+bratiGIT@users.noreply.github.com>
2020-06-03 15:27:47 +0530

    Add files via upload

--- scripts/hooks/after_build.js
+++ scripts/hooks/after_build.js
@@ -1,3 +1,38 @@
+// /**
+//   Copyright (c) 2015, 2019, Oracle and/or its affiliates.
+//   The Universal Permissive License (UPL), Version 1.0
+// */
+
+// 'use strict';
+
+// const path = require('path');
+// const grunt = require('grunt');
+// //const loadGrunt =require('load-grunt-tasks')(grunt);
+
+// /* module.exports = function (configObj) {
+//   return new Promise((resolve, reject) => {
+//   	console.log("Running after_build hook.");
+//   	resolve();
+//   });
+// }; */
+
+// console.log("Override after_build hook to generate war file"); 
+// module.exports = function (configObj) {
+//   require('load-grunt-config')(grunt, {
+//     configPath: path.join(process.cwd(), 'scripts/grunt/config'),
+//     data: {
+//       appname: path.basename(process.cwd()),  // same as project directory name, accessible with '<%= appname %>'
+//       appdir: 'web',  // accessible with '<%= appdir %>'
+//       distdir: 'release'  // accessible with '<%= distdir %>'
+//     }
+//   });
+//  
+//   return new Promise((resolve, reject) => {
+//     console.log("Running after_build hook.");
+//     grunt.tasks(['war']);
+//     resolve();
+//   });
+// };
 /**
   Copyright (c) 2015, 2019, Oracle and/or its affiliates.
   The Universal Permissive License (UPL), Version 1.0
@@ -11,8 +46,8 @@ const grunt = require('grunt');
 console.log("[after_build]: Entering hook");
 
 module.exports = function (configObj) {
-
-  if (configObj.buildType === "release") {
+  console.log(configObj);  
+  // if (configObj.buildType === "release") {
     console.log("[after_build]: Create WAR file for release");
     require('load-grunt-config')(grunt, {
       configPath: path.join(process.cwd(), 'scripts/grunt/config'),
@@ -29,12 +64,12 @@ module.exports = function (configObj) {
       resolve();
       console.log("[after_build]: Exiting hook after war generation");
     });
-  } else {
+  // } else {
 
-    return new Promise((resolve, reject) => {
-      console.log("Running after_build hook.");
-      resolve();
-      console.log("[after_build]: Exiting hook");
-    });
-  };
+  //   return new Promise((resolve, reject) => {
+  //     console.log("Running after_build hook.");
+  //     resolve();
+  //     console.log("[after_build]: Exiting hook");
+  //   });
+  // };
 };
